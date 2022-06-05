5da199e3f24eece8c4ab69dbeeac4675a3a4d2ac
vandeurenglenn <vandeurenglenn@gmail.com>
2022-06-05 15:07:38 +0200

    remove browser field from package

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,4 @@ export default [{
 		format: 'cjs',
 		sourcemap: false
 	}
-}, {
-	input: 'src/generate-account.js',
-	output: {
-		dir: './dist/module',
-		format: 'es',
-		sourcemap: false
-	}
 }]
